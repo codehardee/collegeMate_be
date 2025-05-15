ec12fbe75e3c3c26dbd3823e84075869c2688928
Heetraval <heetraval26@gmail.com>
2025-05-14 23:52:39 -0400

    Manually cleaned requirements.txt

--- college_mate/settings.py
+++ college_mate/settings.py
@@ -34,7 +34,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["collegemate-be-1.onrender.com", "localhost", "127.0.0.1"]
+
 
 
 # Application definition
