a949dbb5d590265c35ecb5622e04c4f12bd7d576
Heetraval <heetraval26@gmail.com>
2025-06-25 15:10:06 -0400

    Add Render domain to ALLOWED_HOSTS

--- college_mate/settings.py
+++ college_mate/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["collegemate-be-1.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["collegemate-be-71uq.onrender.com", "localhost", "127.0.0.1"]
 
 
 
